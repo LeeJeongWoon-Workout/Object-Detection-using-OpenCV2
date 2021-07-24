f4994fd95381d9d0db60102c20d923ea0c73eb31
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-07-25 07:01:47 +0900

    Update get_started.py

--- get_started.py
+++ get_started.py
@@ -1,43 +1,40 @@
+# 원본 이미지가 Faster RCNN기반 네트웍으로 입력 시 resize됨. 
+# scaling된 이미지 기반으로 bounding box 위치가 예측 되므로 이를 다시 원복하기 위해 원본 이미지 shape정보 필요
+rows = img.shape[0]
+cols = img.shape[1]
+# cv2의 rectangle()은 인자로 들어온 이미지 배열에 직접 사각형을 업데이트 하므로 그림 표현을 위한 별도의 이미지 배열 생성. 
+draw_img = img.copy()
+
+# 원본 이미지 배열 BGR을 RGB로 변환하여 배열 입력. Tensorflow Faster RCNN은 마지막 classification layer가 Dense가 아니여서 size를 고정할 필요는 없음.  
+cv_net.setInput(cv2.dnn.blobFromImage(img, swapRB=True, crop=False))
+#cv_net 에 데이터를 넣기 전에 img를 cv2.dnn.blobFromImage로 수정 여부를 결정하고 크기가 0~1로 resize 된다.(Normalize)
+# Object Detection 수행하여 결과를 cvOut으로 반환 
+cv_out = cv_net.forward()
+print(cv_out.shape)
+
 # bounding box의 테두리와 caption 글자색 지정
 green_color=(0, 255, 0)
 red_color=(0, 0, 255)
 
-while True:
+# detected 된 object들을 iteration 하면서 정보 추출
+for detection in cv_out[0,0,:,:]:  #[아무정보X,Class_id,Score,좌상 x,좌상y,우하x,우하y]
+    score = float(detection[2])
+    class_id = int(detection[1])
+    # detected된 object들의 score가 0.5 이상만 추출
+    if score > 0.5:
+        # detected된 object들은 scale된 기준으로 예측되었으므로 다시 원본 이미지 비율로 계산
+        left = detection[3] * cols
+        top = detection[4] * rows
+        right = detection[5] * cols
+        bottom = detection[6] * rows
+        # labels_to_names_seq 딕셔너리로 class_id값을 클래스명으로 변경.
+        caption = "{}: {:.4f}".format(labels_to_names_0[class_id], score)
+        print(caption)
+        #cv2.rectangle()은 인자로 들어온 draw_img에 사각형을 그림. 위치 인자는 반드시 정수형.
+        cv2.rectangle(draw_img, (int(left), int(top)), (int(right), int(bottom)), color=green_color, thickness=2)
+        cv2.putText(draw_img, caption, (int(left), int(top - 5)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, red_color, 1) #(이미지,텍스트,x좌표,y좌표,폰트형식,폰트크기,글색깔,굵기)
 
-    hasFrame, img_frame = cap.read()
-    if not hasFrame:
-        print('더 이상 처리할 frame이 없습니다.')
-        break
+img_rgb = cv2.cvtColor(draw_img, cv2.COLOR_BGR2RGB)
 
-    rows = img_frame.shape[0]
-    cols = img_frame.shape[1]
-    # 원본 이미지 배열 BGR을 RGB로 변환하여 배열 입력
-    cv_net.setInput(cv2.dnn.blobFromImage(img_frame,  swapRB=True, crop=False))
-    
-    start= time.time()
-    # Object Detection 수행하여 결과를 cv_out으로 반환 
-    cv_out = cv_net.forward()
-    frame_index = 0
-    # detected 된 object들을 iteration 하면서 정보 추출
-    for detection in cv_out[0,0,:,:]:  #7 demension [no meaning,class_id,socre,location4]
-        score = float(detection[2])
-        class_id = int(detection[1])
-        # detected된 object들의 score가 0.5 이상만 추출
-        if score > 0.5:
-            # detected된 object들은 scale된 기준으로 예측되었으므로 다시 원본 이미지 비율로 계산
-            left = detection[3] * cols
-            top = detection[4] * rows
-            right = detection[5] * cols
-            bottom = detection[6] * rows
-            # labels_to_names_0딕셔너리로 class_id값을 클래스명으로 변경.
-            caption = "{}: {:.4f}".format(labels_to_names_0[class_id], score)
-            #print(class_id, caption)
-            #cv2.rectangle()은 인자로 들어온 draw_img에 사각형을 그림. 위치 인자는 반드시 정수형.
-            cv2.rectangle(img_frame, (int(left), int(top)), (int(right), int(bottom)), color=green_color, thickness=2)
-            cv2.putText(img_frame, caption, (int(left), int(top - 5)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color, 1)
-    print('Detection 수행 시간:', round(time.time()-start, 2),'초')
-    vid_writer.write(img_frame)
-# end of while loop
-
-vid_writer.release()
-cap.release()   
+plt.figure(figsize=(12, 12))
+plt.imshow(img_rgb)
