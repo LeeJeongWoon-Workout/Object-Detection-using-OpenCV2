fc6f366514802e30f4607076cc70cf8c385824df
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-07-25 07:07:50 +0900

    Update Video_Object_Detection.py

--- Video_Object_Detection.py
+++ Video_Object_Detection.py
@@ -14,14 +14,14 @@ print('총 Frame 갯수:', frame_cnt)
 video_input_path = '/content/data/Jonh_Wick_small.mp4'
 video_output_path = './data/John_Wick_small_cv01.mp4' #Obejct_Detection 이 끝난 영상을 저장할 위치
 
-cap = cv2.VideoCapture(video_input_path)
+cap = cv2.VideoCapture(video_input_path) #cv2 내장함수 VideoCapture가 프레임 별로 사진을 캡쳐한다.
 
 codec = cv2.VideoWriter_fourcc(*'XVID') #XVID 형식으로 작성
 
 vid_size = (round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))) #크기 설정 크기는 원본 영상 파일 유지 
 vid_fps = cap.get(cv2.CAP_PROP_FPS ) #프레임 설정 원본 영상 프레임 유지
     
-vid_writer = cv2.VideoWriter(video_output_path, codec, vid_fps, vid_size) #저장위치,작성할형식,프레임,크기 저장 
+vid_writer = cv2.VideoWriter(video_output_path, codec, vid_fps, vid_size) #저장위치,작성할형식,프레임,크기 저장 Object_Detection이 끝난 영상을 어떻게 배포할 것인지 결정한다.
 
 frame_cnt = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 print('총 Frame 갯수:', frame_cnt)
@@ -37,6 +37,7 @@ while True:
         print('더 이상 처리할 frame이 없습니다.')
         break
 
+    #아래 과정은 단일 사진의 Object_Detection과 똑같다.   
     rows = img_frame.shape[0]
     cols = img_frame.shape[1]
     # 원본 이미지 배열 BGR을 RGB로 변환하여 배열 입력
@@ -64,7 +65,7 @@ while True:
             cv2.rectangle(img_frame, (int(left), int(top)), (int(right), int(bottom)), color=green_color, thickness=2)
             cv2.putText(img_frame, caption, (int(left), int(top - 5)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color, 1)
     print('Detection 수행 시간:', round(time.time()-start, 2),'초')
-    vid_writer.write(img_frame)
+    vid_writer.write(img_frame) #Object-Detection이 끝난 영상을 vid_writer.write를 통해 VideoWriter에 설정된 정보대로 수정한다.
 # end of while loop
 
 vid_writer.release() # 이런 형식으로
