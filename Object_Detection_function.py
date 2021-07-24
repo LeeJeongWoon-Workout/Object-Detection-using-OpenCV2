f07af05f608d486c42b7e4bf713d72531d8fb630
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-07-25 06:28:04 +0900

    Create Object_Detection_function.py

---
+++ Object_Detection_function.py
@@ -0,0 +1,42 @@
+import time
+
+def get_detected_img(cv_net, img_array, score_threshold, use_copied_array=True, is_print=True):
+    
+    rows = img_array.shape[0]
+    cols = img_array.shape[1]
+    
+    draw_img = None
+    if use_copied_array:
+        draw_img = img_array.copy()
+    else:
+        draw_img = img_array
+    
+    cv_net.setInput(cv2.dnn.blobFromImage(img_array, swapRB=True, crop=False))
+    
+    start = time.time()
+    cv_out = cv_net.forward()
+    
+    green_color=(0, 255, 0)
+    red_color=(0, 0, 255)
+
+    # detected 된 object들을 iteration 하면서 정보 추출
+    for detection in cv_out[0,0,:,:]:
+        score = float(detection[2])
+        class_id = int(detection[1])
+        # detected된 object들의 score가 함수 인자로 들어온 score_threshold 이상만 추출
+        if score > score_threshold:
+            # detected된 object들은 scale된 기준으로 예측되었으므로 다시 원본 이미지 비율로 계산
+            left = detection[3] * cols
+            top = detection[4] * rows
+            right = detection[5] * cols
+            bottom = detection[6] * rows
+            # labels_to_names 딕셔너리로 class_id값을 클래스명으로 변경. opencv에서는 class_id + 1로 매핑해야함.
+            caption = "{}: {:.4f}".format(labels_to_names_0[class_id], score)
+            print(caption)
+            #cv2.rectangle()은 인자로 들어온 draw_img에 사각형을 그림. 위치 인자는 반드시 정수형.
+            cv2.rectangle(draw_img, (int(left), int(top)), (int(right), int(bottom)), color=green_color, thickness=2)
+            cv2.putText(draw_img, caption, (int(left), int(top - 5)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, red_color, 1)
+    if is_print:
+        print('Detection 수행시간:',round(time.time() - start, 2),"초")
+
+    return draw_img
